6d610634bbbf3336679b7bb6033be16f336ecb59
Salomao <salomao.machado.75@gmail.com>
2023-06-15 23:10:22 -0300

    <feat> Método CriarItem: Criação do método CriarItem implementação do metodo e limpeza do campo após o processo.

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-input',
@@ -9,11 +10,16 @@ export class InputComponent implements OnInit {
 
   valorItem!: string;
 
-  constructor() { }
+  constructor(private listaService: ListaDeCompraService) { }
 
   ngOnInit(): void { }
 
   adicionarItem(){
-    console.log(this.valorItem);
+    this.listaService.adicionarItemNaLista(this.valorItem);
+    this.limparCampo()
+  }
+
+  limparCampo(){
+    this.valorItem = '';
   }
 }
